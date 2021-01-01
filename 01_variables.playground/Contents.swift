06953e7c7ea201aea11f655d3a4f5c2de15d8a86
Ali Eren Ekinci <mralierenekinci@gmail.com>
2021-01-01 21:25:52 +0300

    Added Variables and DataTypes MyPlayground

---
+++ 01_variables.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// Bu bir yorum satırıdır.
+
+
+//Değişken - Variable
+
+var name = "Ali Eren" // String
+var surname = "Ekinci"
+var age = 201 // int
+
+age = 21
+print(name,surname,age)
+
+var variableOne = 0 // camelCase (Genellikle swifte bu şekilde yapılıyorn )
+var variable_two = 0 // snake_case
+
+
+var userName = "Ali Eren Ekinci" // iki kelimelik değişken adı
+userName = "alierenekinci"
+
+print("Ad:", name, "Soyad:", surname, "Yaş", age, "Kullanıcı Adı:", userName)
---
+++ 02_dataTypes.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+
+// String - str
+var myStr = "Hello, Github." // var ile değişken oluşturduk
+myStr.lowercased()
+myStr.uppercased()
+myStr.append(" I am Ali Eren")
+myStr.isEmpty
+myStr.count
+var newStr: String = "Welcome 2021 <3"
+var emptyStr: String? // Boş bir string tanımlamak istersek ? işaretiyle bastırız. nil olarak döner. :)
+
+let helloGithubLowerCased = myStr.lowercased()
+let helloGithubUpperCased = myStr.uppercased()
+print(helloGithubUpperCased+"\n"+helloGithubLowerCased)
+
+// Integer - Int
+let myNumberInt = 22 // let ile sabit yaptık
+let newNumberInt:Int = 50
+let emptyNumberInt: Int?
+
+//Float
+let myNumberFloat:Float = 12.0
+myNumberFloat
+
+// Double
+let pi = 3.14 // pi değiştirmeye gerek yok. :D
+let eNumber :Double = 2.71
+
+// Boolen - bool
+var isLoggedIn: Bool = false
+var isUpperCase = true
+
+
+//  Bir kaç farklı uygulama
+var resultZero: Int = 22 + 02
+
+var resultOne:Int = 24 * 4
+
+var resultTwo = 22/5
+
+var resultThree:Double = 22/5
