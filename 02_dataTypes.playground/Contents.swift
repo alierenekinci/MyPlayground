cf746ed01c5c8025ccd6c9b3c2b9c94e10a5d738
Ali Eren Ekinci <mralierenekinci@gmail.com>
2021-01-03 13:30:57 +0300

    types of dataType added

--- 02_dataTypes.playground/Contents.swift
+++ 02_dataTypes.playground/Contents.swift
@@ -15,29 +15,57 @@ let helloGithubLowerCased = myStr.lowercased()
 let helloGithubUpperCased = myStr.uppercased()
 print(helloGithubUpperCased+"\n"+helloGithubLowerCased)
 
+
+
+
 // Integer - Int
 let myNumberInt = 22 // let ile sabit yaptık
 let newNumberInt:Int = 50
 let emptyNumberInt: Int?
 
+// Çeşitleri
+let int8: Int? // 8 bitlik int
+let int16: Int16? // 16 bitlik int
+let int32: Int32?
+let int64: Int64?
+
+
+
+
 //Float
 let myNumberFloat:Float = 12.0
 myNumberFloat
+
+// Çeşitleri
+let float: Float?
+let float16: Float16?
+let float32: Float32?
+let float64: Float64?
+let float80: Float80?
+
+
+
 
 // Double
 let pi = 3.14 // pi değiştirmeye gerek yok. :D
 let eNumber :Double = 2.71
 
+<<<<<<< HEAD
+// Çeşitleri
+
+let double:Double
+
+
+// Boolen - bool
+=======
 // Boolean - bool
+>>>>>>> eff8ef908e248a0984b4d87ae49c3ce7730d552f
 var isLoggedIn: Bool = false
 var isUpperCase = true
 
 
 //  Bir kaç farklı uygulama
 var resultZero: Int = 22 + 02
-
 var resultOne:Int = 24 * 4
-
 var resultTwo = 22/5
-
 var resultThree:Double = 22/5
