72a9ee0986a2743edbb83fba2110db0505d29775
Ali Eren Ekinci <mralierenekinci@gmail.com>
2021-01-05 19:58:48 +0300

    Added Array Document

--- 03_Array.playground/Contents.swift
+++ 03_Array.playground/Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
 
-var str = "Hello, playground"
+
+// Koleksiyonlar, Collection
+
+// Array
+var expArray = ["Ali Eren Ekinci","Osman Baki Ekinci","Osman","Ali"]
+expArray[0]
+
+expArray[1].uppercased()
+
+expArray[2] = "Osman Baki"
+
+var mixArray = [100,20,"Yazı",":/"] as [Any] // as -> casting any -> any object
+var newArray = mixArray[2] as! String
+
+mixArray.append(2)
+mixArray.capacity
+mixArray.count
+mixArray.remove(at: 1)
+mixArray[mixArray.count-1]
+mixArray.last
+
+
+var numberArray = [0,3,4,5.2,3,4]
+numberArray.sort()
+
+
