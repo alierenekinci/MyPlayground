b3d4ee4e5ab606aee3a2ce01cdd106a23abf5b6f
Ali Eren Ekinci <mralierenekinci@gmail.com>
2021-01-06 10:46:30 +0300

    Added While Loop

---
+++ 06_whileLoop.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+// While Loop
+
+// Mantıksal Operatörlar
+2 == 2
+5 == 4
+5 != 4
+3 < 5
+3 < 2
+2 > 1
+2 >= 2
+10 <= 10
+
+var x : Double = 0
+while true {
+    x += 1
+    if (x == 10){
+        print(x)
+        break
+    }
+}
+
+var y: Double = 1.0
+while y < 10 {
+    y *= 1.16
+    print(y)
+    
+}
+ 
